ee4a4ba8ca1863a6babb0a4b8596028ae5a9fb5e
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-20 15:57:20 +0530

    add back to top button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,52 @@
-    // --------nav-bar
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".menulist");
-    hamburger.addEventListener("click", mobileMenu);
-    const Navlink  = document.querySelectorAll(".nav-link");
-    Navlink.forEach( e  => e.addEventListener("click" , ()=>{
-        hamburger.classList.toggle("nav-active");
-        document.body.classList.remove("overflow-hidden");
-        navMenu.classList.toggle("nav-active");
-    }))
-    function mobileMenu() {
-      hamburger.classList.toggle("nav-active");
-      document.body.classList.toggle("overflow-hidden");
-      navMenu.classList.toggle("nav-active");
-    }
+// --------nav-bar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menulist");
+hamburger.addEventListener("click", mobileMenu);
+const Navlink = document.querySelectorAll(".nav-link");
+Navlink.forEach(e => e.addEventListener("click", () => {
+  hamburger.classList.toggle("nav-active");
+  document.body.classList.remove("overflow-hidden");
+  navMenu.classList.toggle("nav-active");
+}))
+function mobileMenu() {
+  hamburger.classList.toggle("nav-active");
+  document.body.classList.toggle("overflow-hidden");
+  navMenu.classList.toggle("nav-active");
+}
 
 
 
-    
+//------------preloder
+// document.addEventListener("DOMContentLoaded" , function (){
+//   window.addEventListener("load" , function (){
+//     setTimeout(function (){
+//       let loadingOverlay = document.getElementById("loadingOverlay");
+//      loadingOverlay.style.dispaly = "none";
+//      document.querySelector("body").classList.remove("overflow-hidden");
+
+//     }, 3000);
+//   });
+// });
+
+
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
