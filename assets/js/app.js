d5ada9fe18e35a18bb5eae7a6dd9a9a26dce3738
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-21 09:37:50 +0530

    add preloader

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,15 @@ function mobileMenu() {
 //   });
 // });
 
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
+  });
+});
 
 
 
