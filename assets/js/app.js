13b256cfb1c8f5a13b7f23f59118c46a44e28517
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-21 14:14:46 +0530

    minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
       let loadingOverlay = document.getElementById("loadingOverlay");
       loadingOverlay.style.display = "none";
       document.querySelector("body").classList.remove("overflow_hidden");
-    }, );
+    }, 3000);
   });
 });
 
